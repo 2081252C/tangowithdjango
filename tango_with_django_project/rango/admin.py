0e9833db591f89b5d1a7c1e00a98e5a5d0711f20
unknown <2081252C@BO706-43.ad.dcs.gla.ac.uk>
2016-01-29 16:42:17 +0000

    Chapter 6 completed.

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+admin.site.register(Category)
+
+class PageModel(admin.ModelAdmin):
+    list_display = ('category', 'title', 'url')
+
+admin.site.register(Page, PageModel)
